6c3e80a453140694f66681a229b89873818090db
jaketrent <trent.jake@gmail.com>
2013-02-13 14:01:13 -0700

    more angular toggle (in-mem state); convert board to store state on client

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,33 @@
 var mm = angular.module('mm', []);
 
 mm.controller('BoardCtrl', ['$scope', '$http', function ($scope, $http) {
+
+  function convert_board(board) {
+    var conv_board = {
+      cells: []
+    };
+    _(board).each(function (row, rowIndx) {
+      conv_board.cells.push([]); // new row
+      _(row).each(function (col, colIndx) {
+        conv_board.cells[rowIndx].push({
+          val: col,
+          visible: false
+        });
+      });
+    });
+    return conv_board;
+  }
+
   $http.get('/ws/board/16/16/easy')
     .success(function (data) {
-      $scope.board = data;
+      $scope.board = convert_board(data);
     })
     .error(function (data) {
       alert('Error!');
     });
 
   $scope.toggle = function ($event, cell) {
-    angular.element($event.target).children('.result').toggleClass('hide');
+    cell.visible = true;
   };
 }]);
 
